5c4244d69f5b157686eaa2a0d081dc0ef6424240
Rafid-tawhid <rafidtawhid@gmail.com>
2025-03-14 23:56:08 +0600

    show food items

--- lib/screen/food_items_screen.dart
+++ lib/screen/food_items_screen.dart
@@ -24,14 +24,19 @@ class FoodItemListScreen extends ConsumerWidget {
           if (foodItems.isEmpty) {
             return const Center(child: Text("No food items available."));
           }
-          return ListView.builder(
+          return GridView.builder(
             padding: const EdgeInsets.all(16.0),
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2, // Number of columns
+              crossAxisSpacing: 16.0,
+              mainAxisSpacing: 16.0,
+              childAspectRatio: 0.75, // Adjust height relative to width
+            ),
             itemCount: foodItems.length,
             itemBuilder: (context, index) {
               final foodItem = foodItems[index];
 
               return Card(
-                margin: const EdgeInsets.only(bottom: 16.0),
                 elevation: 3,
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(16.0),
@@ -39,61 +44,75 @@ class FoodItemListScreen extends ConsumerWidget {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    ClipRRect(
-                      borderRadius: const BorderRadius.only(
-                        topLeft: Radius.circular(16.0),
-                        topRight: Radius.circular(16.0),
-                      ),
-                      child: Image.network(
-                        foodItem.image,
-                        height: 150,
-                        width: double.infinity,
-                        fit: BoxFit.cover,
-                      ),
+                    Stack(
+                      children: [
+
+                        ClipRRect(
+                          borderRadius: const BorderRadius.only(
+                            topLeft: Radius.circular(16.0),
+                            topRight: Radius.circular(16.0),
+                          ),
+                          child: Image.network(
+                            foodItem.image,
+                            height: 120,
+                            width: double.infinity,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        Positioned(
+                          top: 0,
+                          right: 0,
+                          child:Container(
+                            padding: const EdgeInsets.symmetric(horizontal: 6,vertical: 2),
+                            decoration: BoxDecoration(
+                              borderRadius: const BorderRadius.only(topRight: Radius.circular(12),bottomLeft:Radius.circular(12) ),
+                              color: foodItem.isAvailable
+                                  ? Colors.green
+                                  : Colors.red,
+                            ),
+                            child: Text(
+                              foodItem.isAvailable ? "Available" : "Not Available",
+                              style: TextStyle(
+                                color: foodItem.isAvailable ? Colors.white : Colors.white,
+                              ),
+                            ),
+                          ),
+
+                        ),
+                      ],
                     ),
                     Padding(
-                      padding: const EdgeInsets.all(12.0),
+                      padding: const EdgeInsets.all(8.0),
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           Text(
                             foodItem.name,
                             style: const TextStyle(
-                              fontSize: 18,
+                              fontSize: 16,
                               fontWeight: FontWeight.bold,
                             ),
-                          ),
-                          const SizedBox(height: 8),
-                          Text(
-                            foodItem.description,
-                            maxLines: 2,
+                            maxLines: 1,
                             overflow: TextOverflow.ellipsis,
                           ),
-                          const SizedBox(height: 8),
-                          Text("Price: \$${foodItem.price.toStringAsFixed(2)}"),
                           const SizedBox(height: 4),
-                          Text("Preparation Time: ${foodItem.preparationTime} mins"),
-                          const SizedBox(height: 8),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Chip(
-                                label: Text(
-                                  foodItem.isAvailable ? "Available" : "Not Available",
-                                  style: TextStyle(
-                                    color: foodItem.isAvailable ? Colors.green : Colors.red,
-                                  ),
-                                ),
-                                backgroundColor: foodItem.isAvailable
-                                    ? Colors.green[50]
-                                    : Colors.red[50],
-                              ),
-                              Text(
-                                foodItem.categoryName,
-                                style: const TextStyle(fontStyle: FontStyle.italic),
-                              ),
-                            ],
+                          // Text(
+                          //   foodItem.description,
+                          //   maxLines: 2,
+                          //   overflow: TextOverflow.ellipsis,
+                          //   style: const TextStyle(fontSize: 12),
+                          // ),
+                          const SizedBox(height: 4),
+                          Text(
+                            "Price: \$${foodItem.price.toStringAsFixed(2)}",
+                            style: const TextStyle(fontSize: 12),
                           ),
+                          Text(
+                            "Prep Time: ${foodItem.preparationTime} mins",
+                            style: const TextStyle(fontSize: 12),
+                          ),
+                          const SizedBox(height: 4),
+
                         ],
                       ),
                     ),
